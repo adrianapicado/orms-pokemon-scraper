2d8c1f2f56e1effa8a72f52c0b1f8170c7ecaebd
adriana <picadoadriana7@gmail.com>
2021-01-11 19:42:33 -0500

    commit tree 65f24166a8e3e8bf756696ff4d3cf1de243bce3e

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,39 @@
 class Pokemon
-end
+ 
+    attr_reader :id, :name, :type, :hp, :db
+    @@all = []
+
+    def initialize (id:, name:, type:, hp: nil, db:)
+    @id = id
+    @name = name
+    @type = type
+    @hp = hp
+    @db = db
+    @@all << self
+    end
+  
+    def self.save(name, type, db)
+      sql = <<-SQL
+        INSERT INTO pokemon (name, type) VALUES (?, ?);
+      SQL
+      
+      db.execute(sql, [name, type])
+      
+    end
+  
+    def self.find(id, database_connection)
+        pokemon = database_connection.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+        name = pokemon[1]
+        type = pokemon[2]
+        hp = pokemon[3]
+    
+        pokemon_inst = Pokemon.new(id: id, name: name, type: type, hp: hp, db: database_connection)
+      end
+  
+    def alter_hp(new_hp)
+      sql = <<-SQL
+        UPDATE pokemon SET hp = ? WHERE id = ?;
+      SQL
+      db.execute(sql, [new_hp, id])
+      end
+  end
